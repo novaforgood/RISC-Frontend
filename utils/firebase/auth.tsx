0eb60b7e71a8978abe031fb29970ab0d48703879
Alyssa Wang <alywang27@g.ucla.edu>
2021-04-23 23:04:21 -0700

    initial firebase setup

---
+++ utils/firebase/auth.tsx
@@ -0,0 +1,103 @@
+import { createContext, Context, useContext, useState, useEffect } from 'react';
+import firebase from './firebase';
+
+// wtf this file do
+
+interface Auth {
+    uid: string;
+    email: string | null;
+    name: string | null;
+    token: string | null;
+}
+
+interface AuthContext {
+    auth: Auth | null;
+    loading: boolean;
+    signInWithGoogle: () => Promise<void>;
+    signOut: () => Promise<void>;
+}
+
+const formatAuth = (user: firebase.User): Auth => ({
+    uid: user.uid,
+    email: user.email,
+    name: user.displayName,
+    token: null
+});
+  
+  // Create context with a default state.
+const authContext: Context<AuthContext> = createContext<AuthContext>({
+    auth: null,
+    loading: true,
+    signInWithGoogle: async () => {},
+    signOut: async () => {}
+}); 
+
+function useProvideAuth() {
+    const [auth, setAuth] = useState<Auth | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    /**
+     * Callback function used for firebase.auth.onAuthStateChanged().
+     * Takes the user object returned and formats it for my state.
+     * We fetch the idToken and append it to my auth state and store it.
+     */
+    const authStateChanged = async (authState: firebase.User | null) => {
+        if (authState) {
+            const formattedAuth = formatAuth(authState);
+            formattedAuth.token = await authState.getIdToken();
+            setAuth(formattedAuth);
+            setLoading(false);
+        }
+    };
+
+    /**
+     * Callback function used for response from firebase OAuth.
+     * Store user object returned in firestore.
+     * @param firebase User Credential
+     */
+    const signedIn = async (resp: firebase.auth.UserCredential) => {
+        // Format user into my required state.
+        const storeUser = formatAuth(resp.user);
+        // firestore database function
+        createUser(storeUser.uid, storeUser); // TODO: Ask Kevin how this should be actually implemented
+    };
+
+    /**
+     * Callback for when firebase signOut.
+     * Sets auth state to null and loading to true.
+     */
+    const clear = () => {
+        setAuth(null);
+        setLoading(true);
+    };
+
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(signedIn);
+    };
+
+    const signOut = () => {
+        return firebase.auth().signOut().then(clear);
+    };
+
+    useEffect(() => {
+        const unsubscribe = firebase.auth().onAuthStateChanged(authStateChanged);
+        return () => unsubscribe();
+    }, []);
+
+    // returns state values and callbacks for signIn and signOut.
+    return {
+        auth,
+        loading,
+        signInWithGoogle,
+        signOut
+    };
+}
+
+export function AuthProvider({ children }: any) {
+    const auth = useProvideAuth();
+    return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
+
+export const useAuth = () => useContext(authContext);
+  
