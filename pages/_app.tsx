16f0f6692644ff0ab7a6bbd0ab428d382e5a8ded
Alyssa Wang <alywang27@g.ucla.edu>
2021-04-23 23:04:21 -0700

    initial firebase setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AppProps } from "next/app";
+import { AuthProvider } from "../utils/firebase/auth";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
-const IndexPage = () => (
+import { useAuth } from "../utils/firebase/auth"
+
+const IndexPage = () => {
+  const { auth, signInWithGoogle, signOut } = useAuth();
+  return (
   <>
     <button onClick={() => console.log("hello world")}>hello world</button>
+    {auth ? 
+        <button onClick={() => signOut()}>Sign Out</button>
+      : <button onClick={() => signInWithGoogle()}>Sign In</button>
+    }
   </>
-);
+)};
 
 export default IndexPage;
