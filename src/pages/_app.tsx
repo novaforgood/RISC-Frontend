fb6e04eaf0ec6db8047fed21514badb9cdb24556
Max Wu <chengmaxwu@gmail.com>
2021-05-03 02:31:53 -0700

    Layout component that persists on route change
    * SampleLayout does not rerender when route changes
    * Modified default Next.js page type to add .getLayout()

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,23 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 import "tailwindcss/tailwind.css";
 
+import Page from "../types/Page";
+
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
 
-function MyApp({ Component, pageProps }: AppProps) {
+type CustomAppProps = AppProps & {
+  Component: Page;
+};
+
+function MyApp({ Component, pageProps }: CustomAppProps) {
+  const getLayout = Component.getLayout || ((page: any) => page);
+
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </ApolloProvider>
   );
 }
