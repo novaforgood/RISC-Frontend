592079e3fef50cce228358f5d3f7bbfa767dc316
Kevin Li <kevinli120@yahoo.com>
2021-05-26 17:54:06 -0700

    Add authorization header to Apollo requests

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
-import { AppProps } from "next/app";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, concat, HttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { ApolloProvider } from "@apollo/client/react";
-import { AuthProvider } from "../../utils/firebase/auth";
+import { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
+import { AuthProvider } from "../../utils/firebase/auth";
+import firebase from "../../utils/firebase/firebase";
+
+const httpLink = new HttpLink({ uri: process.env.NEXT_PUBLIC_API_URL });
+
+const authLink = setContext(async (_, { headers, ...context }) => {
+  const token = await firebase
+    .auth()
+    .currentUser?.getIdToken(/* forceRefresh */ false);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+    ...context,
+  };
+});
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL,
+  link: concat(authLink, httpLink),
   cache: new InMemoryCache(),
 });
 
