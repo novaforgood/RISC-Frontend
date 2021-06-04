9fb575ba81a8db4b44a8e4c234f39ce36679afd1
Max Wu <chengmaxwu@gmail.com>
2021-06-04 13:33:05 -0700

    Calendar component code cleanup
    * data.ts to store month and weekday related data
    * utils.ts to store utility functions

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,7 +1,9 @@
 import classNames from "classnames";
 import React, { useEffect, useMemo, useState } from "react";
 import { Text } from "../../components/atomic";
+import { monthNames, weekdayNamesAbbreviated } from "./data";
 import { Arrow } from "./icons";
+import { dateDiffInDays, getDaysInThisMonth } from "./utils";
 
 // Types
 
@@ -27,11 +29,11 @@ interface CalendarProps {
   availabilities: Availabilities;
   selectedDay: Date | null;
 }
-const Calendar: React.FC<CalendarProps> = ({
+const Calendar = ({
   onSelect = () => {},
   selectedDay,
   availabilities,
-}) => {
+}: CalendarProps) => {
   const [month, setMonth] = useState(initMonth);
   const [year, setYear] = useState(initYear);
   const [days, setDays] = useState<Date[]>([]);
@@ -43,7 +45,7 @@ const Calendar: React.FC<CalendarProps> = ({
 
   if (!days) return <></>;
 
-  // isOverrided: [idx in date array] => date has an override
+  // isOverrided: Map from [idx in date array] => [if date has an override]
   let isOverrided = useMemo(() => {
     const ret: { [key: number]: boolean } = {};
     if (!days || days.length == 0) return ret;
@@ -140,100 +142,3 @@ const Calendar: React.FC<CalendarProps> = ({
 };
 
 export default Calendar;
-
-const weekdayNamesAbbreviated = [
-  "Sun",
-  "Mon",
-  "Tue",
-  "Wed",
-  "Thur",
-  "Fri",
-  "Sat",
-];
-const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const monthNamesAbbreviated = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-const weekdayNames = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-// Avoid typescript errors for now
-weekdayNames;
-monthNamesAbbreviated;
-
-/**
- *
- * @param a
- * @param b
- * @returns Difference in days between [a] and [b]
- */
-function dateDiffInDays(a: Date, b: Date) {
-  // Discard the time and time-zone information.
-  const MS_PER_DAY = 1000 * 60 * 60 * 24;
-  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-  return Math.floor((utc2 - utc1) / MS_PER_DAY);
-}
-
-/**
- *
- * @param month
- * @param year
- * @returns List of days in [month] of [year].
- */
-function getDaysInThisMonth(month: number, year: number) {
-  var date = new Date(year, month, 1);
-  var days = [];
-
-  // Add days until previous Sunday
-  var tempDate = new Date(date);
-  tempDate.setDate(date.getDate() - 1);
-  while (tempDate.getDay() != 6) {
-    days.unshift(new Date(tempDate));
-    tempDate.setDate(tempDate.getDate() - 1);
-  }
-
-  while (date.getMonth() === month) {
-    days.push(new Date(date));
-    date.setDate(date.getDate() + 1);
-  }
-
-  // Add days until Saturday
-  while (date.getDay() != 0) {
-    days.push(new Date(date));
-    date.setDate(date.getDate() + 1);
-  }
-  return days;
-}
