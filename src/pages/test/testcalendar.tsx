bf9674bb5d69fa4df44c6f71716dc47f1f2c89a0
Max Wu <chengmaxwu@gmail.com>
2021-06-03 20:06:49 -0700

    Finished calendar styling and functionality
    * Only show days in current month
    * Only select days with timeslots

--- src/pages/test/testcalendar.tsx
+++ src/pages/test/testcalendar.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import Calendar from "../../components/Calendar";
 
+var start = new Date();
+start.setHours(0, 0, 0, 0);
+
 const TestPage = () => {
-  const [selectedDay, setSelectedDay] = useState(new Date());
+  const [selectedDay, setSelectedDay] = useState<Date | null>(null);
   return (
-    <div className="flex justify-center items-center">
+    <div className="w-screen h-screen flex flex-col justify-start items-center">
+      <div className="h-10"></div>
       <Calendar
         onSelect={(day) => {
           setSelectedDay(day);
         }}
         availabilities={SAMPLE_AVAILABILITIES}
         selectedDay={selectedDay}
-      ></Calendar>
+      />
     </div>
   );
 };
@@ -19,14 +23,8 @@ const TestPage = () => {
 export default TestPage;
 
 const SAMPLE_AVAILABILITIES = {
-  weekly: {
-    sunday: [],
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: [],
-    saturday: [],
-  },
-  overrides: [],
+  weekly: [[{ start: new Date(), end: new Date() }], [], [], [], [], [], []], // Sunday thru Saturday
+  overrides: [
+    { date: new Date(), timeslots: [{ start: new Date(), end: new Date() }] },
+  ],
 };
