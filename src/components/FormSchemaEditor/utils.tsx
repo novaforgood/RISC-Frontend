a53e8b222fe7c8f2d04772dccfc8a8d03eb8f8b2
Max Wu <chengmaxwu@gmail.com>
2021-06-05 23:21:47 -0700

    Use <button> tag for accessibility
    * Replaced Form type with Questions[]

--- src/components/FormSchemaEditor/utils.tsx
+++ src/components/FormSchemaEditor/utils.tsx
@@ -1,6 +1,4 @@
 import _ from "lodash";
-import { DraggableLocation } from "react-beautiful-dnd";
-import { Question } from "./index";
 
 /**
  *
@@ -19,34 +17,6 @@ export const reindexItemInList = (
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
-  return result;
-};
-
-/**
- *
- * @param source
- * @param destination
- * @param droppableSource
- * @param droppableDestination
- * @returns a dictionary: sectionID => new set of questions in that section
- *
- * Moves a question from [source] to [destination] based on drag parameters.
- */
-export const moveQuestionBetweenSections = (
-  source: Question[],
-  destination: Question[],
-  droppableSource: DraggableLocation,
-  droppableDestination: DraggableLocation
-) => {
-  const sourceClone = Array.from(source);
-  const destClone = Array.from(destination);
-  const [removed] = sourceClone.splice(droppableSource.index, 1);
-  destClone.splice(droppableDestination.index, 0, removed);
-
-  const result: Record<string, Question[]> = {};
-  result[droppableSource.droppableId] = sourceClone;
-  result[droppableDestination.droppableId] = destClone;
-
   return result;
 };
 
