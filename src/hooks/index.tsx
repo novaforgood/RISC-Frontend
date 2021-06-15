5d976c25b2a3014bd6d39c0dd105601b82454e04
Kevin Li <kevinli120@yahoo.com>
2021-06-15 00:34:19 -0700

    Link availabilities with program and profile

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -2,4 +2,5 @@ export {
   AuthorizationLevel,
   default as useAuthorizationLevel,
 } from "./useAuthorizationLevel";
+export { default as useCurrentProfile } from "./useCurrentProfile";
 export { default as useCurrentProgram } from "./useCurrentProgram";
