af0d4c54b905159ba10d3210bbf239ddc43d2aa4
hcsong213 <51963099+hcsong213@users.noreply.github.com>
2021-06-14 23:01:33 -0700

    Refine Mentor App Styles and Functionality

--- src/pages/program/[slug]/apply.tsx
+++ src/pages/program/[slug]/apply.tsx
@@ -1,5 +1,7 @@
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { Button, Text } from "../../../components/atomic";
+import Form, { dummyForm } from "../../../components/Form";
 import {
   ApplicationType,
   CreateApplicationInput,
@@ -10,17 +12,22 @@ import {
   useAuthorizationLevel,
   useCurrentProgram,
 } from "../../../hooks";
+import RedirectIfNotLoggedIn from "../../../layouts/RedirectIfNotLoggedIn";
 import Page from "../../../types/Page";
 import { useAuth } from "../../../utils/firebase/auth";
-import Form, { dummyForm } from "../../../components/Form";
 
+// Question: Do we want users to see mentor app (behind the modal) before they log in?
+// Urgent: Should check if user applied to be mentor before prior to allowing them to access the form.
 const ProgramApplyPage: Page = (_) => {
   const { user } = useAuth();
+  const authorizationLevel = useAuthorizationLevel();
   const { currentProgram } = useCurrentProgram();
   const [createApplication] = useCreateApplicationMutation();
-  const authorizationLevel = useAuthorizationLevel();
-  const [responses, setResponses] = useState({}); // Should fetch previously saved answers
+  const router = useRouter();
+
+  const [responses] = useState({}); // Should fetch previously saved answers
   const [formChanged, setFormChanged] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   const { Admin, Mentor, Mentee } = AuthorizationLevel;
   if ([Admin, Mentor, Mentee].includes(authorizationLevel))
@@ -35,38 +42,56 @@ const ProgramApplyPage: Page = (_) => {
     );
 
   return (
-    <div className="h-screen w-screen">
-      <div>
-        <Text h2 b>
-          Apply to {currentProgram?.name} as a mentor!
-        </Text>
-      </div>
-      <div className="w-screen">
-        <Form
-          questions={dummyForm} // Should actually fetch form schema
-          responses={responses}
-          onChange={() => {
-            setFormChanged(true);
-          }}
-          onAutosave={(response) => {
-            setResponses(response);
-          }}
-        ></Form>
-        <Button
-          size="small"
-          disabled={!formChanged}
-          onClick={() => {
-            if (!currentProgram || !user) return;
-            const createApplicationInput: CreateApplicationInput = {
-              programId: currentProgram.programId!,
-              applicationJson: JSON.stringify(responses),
-              applicationType: ApplicationType.Mentor,
-            };
-            createApplication({ variables: { data: createApplicationInput } });
-          }}
-        >
-          Apply
-        </Button>
+    <div className="min-w-screen h-screen bg-tertiary">
+      {/* Nav bar goes here */}
+      <div
+        className={"max-w-4xl mx-auto p-10" + (formSubmitted ? " hidden" : "")}
+      >
+        <div className="mt-9">
+          <Text h1 b>
+            {currentProgram?.name}
+          </Text>
+        </div>
+        <div className="mt-4">
+          <Text h3>Mentor Application</Text>
+        </div>
+        <div className="mt-6 mx-10">
+          <Form
+            questions={dummyForm} // Should actually fetch form schema
+            responses={responses}
+            onChange={() => {
+              setFormChanged(true);
+            }}
+            // onAutosave={(response) => {
+            //   setResponses(response);
+            // }}
+          ></Form>
+          <div className="flex justify-between mt-10">
+            <Button variant="inverted">Back</Button>
+            <Button
+              disabled={!formChanged}
+              onClick={() => {
+                if (!currentProgram || !user) return; // Should show an error message
+                const createApplicationInput: CreateApplicationInput = {
+                  programId: currentProgram.programId!,
+                  applicationJson: JSON.stringify(responses),
+                  applicationType: ApplicationType.Mentor,
+                };
+                createApplication({
+                  variables: { data: createApplicationInput },
+                });
+                setFormSubmitted(true);
+              }}
+            >
+              Submit
+            </Button>
+            {user == null ? (
+              <RedirectIfNotLoggedIn
+                pathAfterLoggingIn={`${router.asPath}`}
+              ></RedirectIfNotLoggedIn>
+            ) : null}
+          </div>
+        </div>
       </div>
     </div>
   );
