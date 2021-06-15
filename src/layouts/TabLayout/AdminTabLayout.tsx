4d7098b877e859a771bbbbe973466050dfdbf9d5
Max Wu <chengmaxwu@gmail.com>
2021-06-15 00:13:05 -0700

    Added all admin tabs

--- src/layouts/TabLayout/AdminTabLayout.tsx
+++ src/layouts/TabLayout/AdminTabLayout.tsx
@@ -1,4 +1,4 @@
-import { Clipboard, Home, Smile } from "../../components/icons";
+import { Clipboard, Home, Settings, Smile } from "../../components/icons";
 import TabLayout, { BaseTabLayoutProps, joinPath } from "./TabLayout";
 
 const { PageItem, Dropdown, Separator } = TabLayout;
@@ -10,6 +10,11 @@ const AdminTabLayout: React.FC<BaseTabLayoutProps> = ({
   return (
     <TabLayout currentPageChildren={children}>
       <PageItem label="Homepage" Icon={Home} path={joinPath(basePath)} />
+      <PageItem
+        label="Settings"
+        Icon={Settings}
+        path={joinPath(basePath, "settings")}
+      />
       <Separator />
       <Dropdown label="Mentors" id="admin-mentors">
         <PageItem
@@ -22,15 +27,41 @@ const AdminTabLayout: React.FC<BaseTabLayoutProps> = ({
           Icon={Clipboard}
           path={joinPath(basePath, "mentors", "applications")}
         />
+        <PageItem
+          label="Edit Mentor Application"
+          Icon={Clipboard}
+          path={joinPath(basePath, "mentors", "editapplication")}
+        />
+        <PageItem
+          label="Edit Mentor Profile"
+          Icon={Clipboard}
+          path={joinPath(basePath, "mentors", "editprofile")}
+        />
       </Dropdown>
       <Separator />
       <Dropdown label="Mentees" id="admin-mentees">
+        <PageItem
+          label="View Mentees"
+          Icon={Smile}
+          path={joinPath(basePath, "mentees")}
+        />
         <PageItem
           label="Mentee Applications"
           Icon={Clipboard}
           path={joinPath(basePath, "mentees", "applications")}
         />
+        <PageItem
+          label="Edit Mentee Application"
+          Icon={Clipboard}
+          path={joinPath(basePath, "mentees", "editapplication")}
+        />
+        <PageItem
+          label="Edit Mentee Profile"
+          Icon={Clipboard}
+          path={joinPath(basePath, "mentees", "editprofile")}
+        />
       </Dropdown>
+      <Separator />
     </TabLayout>
   );
 };
