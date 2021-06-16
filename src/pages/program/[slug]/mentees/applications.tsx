9fd2d3be3f49d9f02c3f290ec248b3d9ad178abe
Kevin Li <kevinli120@yahoo.com>
2021-06-15 21:15:05 -0700

    Add review applications page

--- src/pages/program/[slug]/mentees/applications.tsx
+++ src/pages/program/[slug]/mentees/applications.tsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { Text } from "../../../../components/atomic";
+import { ApplicationFilterer } from "../../../../components/ReviewApplications/ApplicationFilterer";
+import { ApplicationType } from "../../../../generated/graphql";
+import { useCurrentProgram } from "../../../../hooks";
 import ChooseTabLayout from "../../../../layouts/ChooseTabLayout";
-import Page from "../../../../types/Page";
 
-const MenteeApplicationsPage: Page = (_) => {
-  return <div>Mentee Application Page</div>;
+const MenteeApplicationsPage = () => {
+  const { currentProgram } = useCurrentProgram();
+
+  if (!currentProgram) {
+    return <></>;
+  }
+
+  return (
+    <div className="bg-tertiary h-screen">
+      <div className="flex flex-col items-center w-5/6 mx-auto h-full">
+        <div className="flex w-5/6 items-center">
+          <Text h1 b>
+            Applications
+          </Text>
+          {/* <div className="flex-1" />
+          <label className="space-x-4">
+            <Text>Accepting Applications</Text>
+            <input type="checkbox" />
+          </label> */}
+        </div>
+        <div className="h-4" />
+        <div className="w-4/5 flex-1">
+          <ApplicationFilterer
+            programId={currentProgram.programId}
+            applicationType={ApplicationType.Mentee}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 MenteeApplicationsPage.getLayout = (page, pageProps) => (
