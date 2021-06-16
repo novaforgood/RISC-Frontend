9ffae6f032c912a5c5e60f314e9a2f2e1bc48d0a
Max Wu <chengmaxwu@gmail.com>
2021-06-15 20:51:17 -0700

    Cleaned up checkbox microinteractions

--- src/components/atomic/Checkbox.tsx
+++ src/components/atomic/Checkbox.tsx
@@ -3,28 +3,42 @@ import React, { InputHTMLAttributes } from "react";
 import { Text } from "../../components/atomic";
 
 type CheckboxProps = InputHTMLAttributes<HTMLInputElement> & {
-  labeltext?: string;
+  label?: string;
+  checked: boolean;
+  onCheck: (checked: boolean) => void;
 };
 
-const Checkbox = ({ className = "", ...props }: CheckboxProps) => {
+const Checkbox = ({
+  className = "",
+  checked,
+  onCheck = () => {},
+  ...props
+}: CheckboxProps) => {
   const checkboxStyles = classNames({
-    ["h-5 w-5 border-2 border-primary rounded appearance-none \
-      hover:bg-darkblue active:border-darkblue active:border-3\
-      checked:bg-primary checked:fill-white -z-1 absolute"]:
+    ["h-5 w-5 border-1.5 border-secondary hover:border-primary rounded appearance-none \
+      cursor-pointer absolute duration-75"]:
       true,
+    ["border-3 bg-primary fill-white"]: checked,
   });
 
+  console.log(checked);
+
   return (
-    <label
-      className="relative cursor-pointer inline-block align-middle"
-      htmlFor={props.id}
-    >
-      <svg className="h-5 w-5 fill-none stroke-white stroke-2 strokeLinecap-round absolute">
+    <div className="relative flex items-center p-4">
+      <svg className="h-5 w-5 fill-none stroke-white stroke-2 strokeLinecap-round absolute z-10 pointer-events-none">
         <path xmlns="http://www.w3.org/2000/svg" d="M 4,11 8,14.5 16,5" />
       </svg>
-      <input type="checkbox" {...props} className={checkboxStyles} />
-      <Text className="ml-6">{props.labeltext}</Text>
-    </label>
+      <input
+        type="checkbox"
+        {...props}
+        className={checkboxStyles}
+        checked={checked}
+        onClick={() => {
+          onCheck(!checked);
+        }}
+      />
+      <Text className="pl-7 select-none">{props.label}</Text>
+    </div>
   );
 };
 
