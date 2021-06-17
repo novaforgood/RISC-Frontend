54dcdc8c8ab3b0de1d8bc154a8bf47e4f1d73d21
Kevin Li <kevinli120@yahoo.com>
2021-06-16 23:24:16 -0700

    Add basic mentor and mentee tab layout

--- src/layouts/TabLayout/MentorTabLayout.tsx
+++ src/layouts/TabLayout/MentorTabLayout.tsx
@@ -1,12 +1,24 @@
 import { Home } from "../../components/icons";
-import TabLayout, { BaseTabLayoutProps } from "./TabLayout";
+import TabLayout, { BaseTabLayoutProps, joinPath } from "./TabLayout";
 
 const { PageItem } = TabLayout;
 
-const MentorTabLayout: React.FC<BaseTabLayoutProps> = ({ children }) => {
+const MentorTabLayout: React.FC<BaseTabLayoutProps> = ({
+  children,
+  basePath,
+}) => {
   return (
     <TabLayout currentPageChildren={children}>
-      <PageItem label="Mentor tab layout" Icon={Home} path="" />
+      <PageItem
+        label="My Availabilities"
+        Icon={Home}
+        path={joinPath(basePath, "availabilities")}
+      />
+      <PageItem
+        label="My Chats"
+        Icon={Home}
+        path={joinPath(basePath, "chat-requests")}
+      />
     </TabLayout>
   );
 };
