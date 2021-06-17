bfd5ff2276800102c514486994f6394f670cb31f
hcsong213 <51963099+hcsong213@users.noreply.github.com>
2021-06-17 13:32:57 -0700

    Finish Frontend of Applications Page (No Functionality)

--- src/pages/my/applications.tsx
+++ src/pages/my/applications.tsx
@@ -9,12 +9,12 @@ const renderApplicationEntries = () => {
 
 const ApplicationsViewer: Page = () => {
   return (
-    <div className="flex justify-center min-w-screen h-screen bg-tertiary">
-      <div>
-        <BackArrow className="h-10 w-10"></BackArrow>
-      </div>
-      <div>
-        <Text h2 b>
+    <div className=" min-w-screen h-screen bg-tertiary">
+      <div></div>
+      <div className="max-w-4xl mx-auto p-10">
+        {/* TODO: My Applications text should be aligned with the Card below it */}
+        <BackArrow className="h-10 w-10 inline"></BackArrow>
+        <Text h2 b className="inline">
           My Applications
         </Text>
         <Card className="flex-shrink mt-10 p-8">
