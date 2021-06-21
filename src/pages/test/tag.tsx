18a4b74007cd90a51fec9e2d23f985a700fe896f
Alyssa Wang <alywang27@gmail.com>
2021-06-20 22:30:48 -0700

    starting to test backend queries

--- src/pages/test/tag.tsx
+++ src/pages/test/tag.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { Button, Card, Tag, Text } from "../../components/atomic";
-import { useGetUsersLazyQuery } from "../../generated/graphql";
+import {
+  useGetUsersLazyQuery,
+  useGetProfileTagsByProgramLazyQuery,
+  useCreateProfileTagMutation,
+  CreateProfileTagInput,
+} from "../../generated/graphql";
 
 const IndexPage = () => {
   const [getUser, { data }] = useGetUsersLazyQuery();
+  const [getTags, { tags }] = useGetProfileTagsByProgramLazyQuery();
+  const [createTag] = useCreateProfileTagMutation();
+
+  let createTagInput: CreateProfileTagInput = {
+    name: "blah",
+    programId: "xfMHQGFjgb0VEKQZtuyKv",
+  };
 
   return (
     <>
@@ -12,11 +24,16 @@ const IndexPage = () => {
         variant="solid"
         onClick={() => {
           getUser();
+          createTag({
+            variables: { data: createTagInput },
+          });
+          getTags({ variables: { programId: "xfMHQGFjgb0VEKQZtuyKv" } });
         }}
       >
         Get users
       </Button>
       <p>{JSON.stringify(data)}</p>
+      <p>{JSON.stringify(tags)}</p>
       <Card className="h-60 w-60">
         <Text>Blaah</Text>
         <div className="h-1"></div>
