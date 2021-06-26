f8b517ef9b3f504b5ead24667b8975d51219616f
hcsong213 <51963099+hcsong213@users.noreply.github.com>
2021-06-26 16:09:08 -0700

    Polish Tag Frontend

--- src/components/atomic/Tag.tsx
+++ src/components/atomic/Tag.tsx
@@ -7,12 +7,14 @@ type TagProps = HTMLAttributes<HTMLElement> & {
 
 const Tag = ({ variant = "solid", className, ...props }: TagProps) => {
   const styles = classNames({
-    "inline-block rounded-md border-2 p-1 m-1 hover:cursor-pointer": true,
+    "inline-block rounded-xl border-2 px-1.5 m-1": true, // hover:cursor-pointer should only be true if there is an onClick behavior
+    "hover:cursor-pointer": onclick,
+    "hover:cursor-default": !onclick,
     "bg-tertiary border-tertiary": variant === "solid",
     "inline-block bg-white border-primary": variant === "outline",
     [`${className}`]: true,
   });
-  return <div {...props} className={styles}></div>;
+  return <div {...props} className={styles}></div>; // There should also be an option to have the 'X'
 };
 
 export default Tag;
