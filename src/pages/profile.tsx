fcbd48597ad91f28202ff20d3ff2811c92e43b6f
Ashley Zhu <ashleyrawr74@gmail.com>
2021-06-30 18:26:11 -0700

    Add publish ability and back button

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Card, Text } from "../components/atomic";
 import {
   useGetMyUserQuery,
@@ -7,34 +7,46 @@ import {
 } from "../generated/graphql";
 import UploadIconWithPreview from "../components/UploadIconWithPreview";
 import TitledInput from "../components/TitledInput";
+import { BackArrow } from "../components/icons";
+import { useRouter } from "next/router";
 
 const GeneralProfile = () => {
   const { data } = useGetMyUserQuery();
   const [updateUser] = useUpdateUserMutation();
-  const [uploadImageAndResize] = useUploadImageAndResizeMutation();
+  const [uploadImageAndResizeMutation] = useUploadImageAndResizeMutation();
   const [modified, setModified] = useState(false);
   const [firstName, setFirstName] = useState(data?.getMyUser.firstName);
   const [lastName, setLastName] = useState(data?.getMyUser.lastName);
-  const [initialSrc, setInitialSrc] = useState(
-    data?.getMyUser.profilePictureUrl
-  );
-  const [profilePicture, setProfilePicture] = useState<File | null>();
-  const [error, setError] = useState("");
-  // const pictureRef = useRef()
 
-  if (!data) return <div>Loading...</div>;
+  const [profilePicture, setProfilePicture] = useState<File | null>();
+  const [src, setSrc] = useState(data?.getMyUser.profilePictureUrl);
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    reset();
+  }, [data?.getMyUser]);
 
   const reset = () => {
-    setModified(false);
-    setFirstName(data.getMyUser.firstName);
-    setLastName(data.getMyUser.lastName);
-    setInitialSrc(data.getMyUser.profilePictureUrl);
-    setError("");
+    if (data) {
+      setModified(false);
+      setFirstName(data.getMyUser.firstName);
+      setLastName(data.getMyUser.lastName);
+      setSrc(data.getMyUser.profilePictureUrl);
+      setProfilePicture(null);
+      setError("");
+    }
   };
 
   return (
     <div className="h-screen bg-tertiary flex flex-col items-center py-10 overflow-y-auto">
       <div className="w-3/4">
+        <button
+          className="cursor-pointer h-max w-max"
+          onClick={() => router.back()}
+        >
+          <BackArrow />
+        </button>
         <div className="flex justify-between items-center">
           <Text h1 b>
             General Profile
@@ -53,17 +65,17 @@ const GeneralProfile = () => {
               disabled={!modified}
               size="small"
               onClick={async () => {
-                let url = data.getMyUser.profilePictureUrl;
+                let url = data?.getMyUser.profilePictureUrl;
                 if (profilePicture) {
-                  let response = await uploadImageAndResize({
+                  let imageUrl = await uploadImageAndResizeMutation({
                     variables: {
                       file: profilePicture,
                       resizeWidth: 256,
                       resizeHeight: 256,
                     },
                   }).catch((err) => setError(err));
-                  if (response.data) {
-                    url = response.data.uploadImage;
+                  if (imageUrl.data) {
+                    url = imageUrl.data.uploadImage;
                   }
                 }
                 updateUser({
@@ -75,6 +87,7 @@ const GeneralProfile = () => {
                     },
                   },
                 });
+                setModified(false);
               }}
             >
               Save
@@ -82,39 +95,48 @@ const GeneralProfile = () => {
           </div>
         </div>
         <div className="h-4" />
-        <Card className="p-10 space-y-4">
-          <Text h3 b>
-            Profile Picture
-          </Text>
-          <UploadIconWithPreview
-            onFileChanged={(file) => {
-              setProfilePicture(file);
-              setInitialSrc("");
-              setModified(true);
-            }}
-            onErrorOccured={setError}
-            initialSrc={initialSrc}
-          />
-          <div className="h-4" />
-          <Text h3 b>
-            About You
-          </Text>
-          <div className="flex space-x-12">
-            <TitledInput
-              title="First Name"
-              placeholder="First Name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+        {data?.getMyUser ? (
+          <Card className="p-10 space-y-4">
+            <Text h3 b>
+              Profile Picture
+            </Text>
+            <UploadIconWithPreview
+              onFileChanged={(file) => {
+                setProfilePicture(file);
+                setModified(true);
+              }}
+              src={src || data.getMyUser.profilePictureUrl}
+              onSrcChange={setSrc}
             />
-            <TitledInput
-              title="Last Name"
-              placeholder="Last Name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </div>
-          <Text className="text-error">{error}</Text>
-        </Card>
+            <div className="h-4" />
+            <Text h3 b>
+              About You
+            </Text>
+            <div className="flex space-x-12">
+              <TitledInput
+                title="First Name"
+                placeholder="First Name"
+                value={firstName || ""}
+                onChange={(e) => {
+                  setModified(true);
+                  setFirstName(e.target.value);
+                }}
+              />
+              <TitledInput
+                title="Last Name"
+                placeholder="Last Name"
+                value={lastName || ""}
+                onChange={(e) => {
+                  setModified(true);
+                  setLastName(e.target.value);
+                }}
+              />
+            </div>
+            <Text className="text-error">{error}</Text>
+          </Card>
+        ) : (
+          <div>Loading</div>
+        )}
       </div>
     </div>
   );
