36b0f90d602f0f70bbf74f4709209c32b8c416eb
hcsong213 <51963099+hcsong213@users.noreply.github.com>
2021-07-05 20:16:29 -0700

    Refine UI according to PR comments

--- src/components/ReviewChats/ChatRequestMutators.tsx
+++ src/components/ReviewChats/ChatRequestMutators.tsx
@@ -1,7 +1,6 @@
 import { Fragment, useState } from "react";
 import { useCancelChatRequestMutation } from "../../generated/graphql";
 import { Input, Text } from "../atomic";
-import CatchUnsavedChangesModal from "../CatchUnsavedChangesModal";
 import SelectOptionModal from "../SelectOptionModal";
 
 type ChatMutatorsProps = {
@@ -9,18 +8,24 @@ type ChatMutatorsProps = {
   chatCanceled: boolean;
 };
 
-const ChatMutators = ({ chatRequestId, chatCanceled }: ChatMutatorsProps) => {
+/**
+ * @summary buttons for canceling and (coming soon) rescheduling chats
+ */
+const ModifyChatRequestModal = ({
+  chatRequestId,
+  chatCanceled,
+}: ChatMutatorsProps) => {
   const [clicked, setClicked] = useState(false);
   const [action, setAction] = useState("");
   const [cancelReason, setCancelReason] = useState("");
-  const [modified, setModified] = useState(false);
-  const [cancelChatRequestMutation] = useCancelChatRequestMutation();
+  const [cancelChatRequestMutation] = useCancelChatRequestMutation({
+    refetchQueries: ["getChatRequests"],
+  });
 
   console.log(chatCanceled);
   return (
     <Fragment>
       {/* This catch is super incomplete bc it doesn't prevent users from closing the modal mid-editting */}
-      <CatchUnsavedChangesModal unsavedChangesExist={modified === true} />
       {!chatCanceled && (
         <button
           onClick={() => {
@@ -39,7 +44,7 @@ const ChatMutators = ({ chatRequestId, chatCanceled }: ChatMutatorsProps) => {
           setClicked(false);
           setAction("");
         }}
-        title={`What is your reason for ${action}ing?`}
+        title={`Let the mentor know your reason for ${action}ing`}
         onPrimaryButtonClick={() => {
           if (action === "cancel")
             cancelChatRequestMutation({
@@ -60,63 +65,13 @@ const ChatMutators = ({ chatRequestId, chatCanceled }: ChatMutatorsProps) => {
         <Input
           placeholder={"Reason for " + action + "ing (optional)"}
           onChange={(e) => {
-            setModified(true);
             setCancelReason(e.target.value);
           }}
           className="w-full"
         />
       </SelectOptionModal>
-      {/* This is the vanilla Modal that I made before I knew of the SelectOptionModal */}
-      {/* <Modal
-        isOpen={clicked}
-        onClose={() => {
-          setClicked(false);
-          setAction("");
-        }}
-      >
-        <div className="p-2 flex flex-col space-y-2 box-border">
-          <Text b>What is your reason for {action}ing?</Text>
-          <Input
-            placeholder={"Reason for " + action + "ing (optional)"}
-            onChange={(e) => {
-              setModified(true);
-              setCancelReason(e.target.value);
-            }}
-            className=""
-          />
-          <div className="space-x-4">
-            <Button
-              variant="inverted"
-              size="small"
-              onClick={() => {
-                if (action === "cancel")
-                  cancelChatRequestMutation({
-                    variables: {
-                      chatRequestId: chatRequestId,
-                      chatCancelMessage: cancelReason,
-                    },
-                  }); //Cancel query
-                if (action === "reschedule")
-                  console.log("Reschedule not ready yet"); //Reschedule query
-                setClicked(false);
-              }}
-            >
-              Submit
-            </Button>
-            <Button
-              variant="solid"
-              size="small"
-              onClick={() => {
-                setClicked(false);
-              }}
-            >
-              Close
-            </Button>
-          </div>
-        </div>
-      </Modal> */}
     </Fragment>
   );
 };
 
-export default ChatMutators;
+export default ModifyChatRequestModal;
